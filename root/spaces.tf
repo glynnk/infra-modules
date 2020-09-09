a9e373dec53b5cb4781197195754bebd9af63d6b
Keith Glynn <glynnk@gmail.com>
2020-09-09 08:08:11 +0100

    added specific bucket for tfstate

--- root/spaces.tf
+++ root/spaces.tf
@@ -2,3 +2,8 @@ resource "digitalocean_spaces_bucket" "bucket" {
   name   = format("%S-bucket", var.root.name)
   region = var.root.bucket_region
 }
+
+resource "digitalocean_spaces_bucket" "bucket" {
+  name   = format("%S-tfstate-bucket", var.root.name)
+  region = var.root.bucket_region
+}
